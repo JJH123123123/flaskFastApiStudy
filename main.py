45930149dacf7beb97fde7b04407f59b4c757dc1
Jin Ho Jeon <34098750+JJH123123123@users.noreply.github.com>
2024-11-21 06:26:19 +0900

    page -59

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -53,3 +53,79 @@ def absolute():
 @app.get('/https')
 def https():
     return f'HTTPS 절대 URL : {url_for("index", _scheme="https", _external=True)}'
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1:int, num2:int):
+    return f'test'
+
+
+@app.route('/int/<int:var>')
+def int_type(var: int):
+    return f'Integer: {var}'
+
+
+@app.route('/float/<float:var>')
+def float_type(var: float):
+    return f'Float: {var}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath: {subpath}'
+
+
+@app.route('/uuid/<uuid:some_id>')
+def show_uuid(some_id):
+    return f'UUID: {some_id}'
+
+
+
+# 요청과 응답
+
+@app.route('/query')
+def query_example():
+    langauage = request.args.get('language')
+    return f"Requested language : {langauage}"
+
+
+@app.route('/items', strict_slashes=False)
+def find_page():
+    page = request.args.get('page', default='1')
+    sort = request.args.get('sort', default='desc')
+
+    # Validate sort parameter
+    if sort not in ['asc', 'desc']:
+        return f"Invalid sort value: {sort}", 400
+    # Rest of your code
+    return f"Requested page: {page} and sort order: {sort}"
+
+
+# serialize
+
+
+@app.route('/json', methods=['GET', 'POST'])
+def json_example():
+    # jsonify
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+
+        return jsonify(data)
+
+    return jsonify({'Hello': ' World!'})
+    # jsonify - >  return current_app.json.response(*args, **kwargs)  # type: ignore[return-value]
+    # -> return self._app.response_class(self.dumps(obj), mimetype="application/json")
+
+
+
+
+@app.route('/response')
+def response_example():
+    # 응답 객체 생성
+
+    resp = make_response("Hello with header", 200)
+
+    resp.headers['Custom-Header'] = 'custom-value'
+
+    return resp
